717e930cf0f74172dae6a9e7ae0efca45b79bc0b
Torleif Nielsen <torleif@hawaii.edu>
2017-07-03 01:37:46 -0700

    Initial commit.

---
+++ test/game_test.exs
@@ -0,0 +1,88 @@
+defmodule GameTest do
+  use ExUnit.Case
+
+  alias Hangman.Game
+
+  test "new_game returns a struct" do
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+    assert List.to_string(game.letters) =~ ~r/[a-z]/
+  end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert ^game = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+    game = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is not already used" do
+    game = Game.new_game()
+    game = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    game = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    moves = [
+      { "w", :good_guess },
+      { "i", :good_guess },
+      { "b", :good_guess },
+      { "l", :good_guess },
+      { "e", :won        },
+    ]
+
+    game = Game.new_game("wibble")
+
+    Enum.reduce(moves, game, fn ({ guess, state }, new_game) ->
+      new_game = Game.make_move(new_game, guess)
+      assert new_game.game_state == state
+      assert new_game.turns_left == 7
+      new_game
+    end)
+  end
+
+  test "bad guess is recognized" do
+    game = Game.new_game("wibble")
+    game = Game.make_move(game, "x")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
+  test "lost game is recognized" do
+    moves =[
+      { "a", :bad_guess },
+      { "b", :bad_guess },
+      { "c", :bad_guess },
+      { "d", :bad_guess },
+      { "e", :bad_guess },
+      { "f", :bad_guess },
+      { "g", :lost      },
+    ]
+
+    game = Game.new_game("w")
+
+    Enum.reduce(moves, game, fn ({ guess, state }, new_game) ->
+      new_game = Game.make_move(new_game, guess)
+      assert new_game.game_state == state
+      new_game
+    end)
+  end
+end
